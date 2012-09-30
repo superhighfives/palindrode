04aa5a922b6c035273fef579bd0998b0297e4dfd
Charlie Gleason <hi@superhighfives.com>
2012-09-30 13:27:07 +1000

    Followed the tutorial, added code.

--- test/palindrode_test.js
+++ test/palindrode_test.js
@@ -20,15 +20,31 @@ var palindrode = require('../lib/palindrode.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
+exports['test'] = {
+  'correctly matches palindrome string': function(test) {
     test.expect(1);
-    // tests here
-    test.equal(palindrode.awesome(), 'awesome', 'should be awesome.');
+    test.ok(palindrode.test('Was it a car or a cat I saw?'));
+    test.done();
+  },
+  'correctly matches non-palindrome strings': function(test) {
+    test.expect(1);
+    test.equal(palindrode.test('This is not a palindrome'), false);
+    test.done();
+  },
+  'returns false when passed blank strings': function(test) {
+    test.expect(1);
+    test.equal(palindrode.test(''), false);
+    test.done();
+  },
+  'returns false when passed only punctuation/spaces': function(test) {
+    test.expect(1);
+    test.equal(palindrode.test(' ?., '), false);
+    test.done();
+  },
+  'returns false when passed non-string values': function(test) {
+    test.expect(2);
+    test.equal(palindrode.test(1234), false, 'should not accept numbers');
+    test.equal(palindrode.test(), false, 'should not accept undefined');
     test.done();
   }
 };
